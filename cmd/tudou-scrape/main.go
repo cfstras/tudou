8d19874f6af64fafd1c400872b3def37e85107ff
Claus Strasburger <claus@strasburger.de>
2014-08-30 22:29:18 +0200

    rainbooooowwws

--- cmd/tudou-scrape/main.go
+++ cmd/tudou-scrape/main.go
@@ -8,7 +8,9 @@ import (
 	"os/user"
 
 	"github.com/crowdmob/goamz/aws"
+	"github.com/crowdmob/goamz/s3"
 	"github.com/crowdmob/goamz/sqs"
+	ct "github.com/daviddengcn/go-colortext"
 )
 
 const (
@@ -19,14 +21,41 @@ const (
 	ErrorNotImpl
 	ErrorGetQueue
 	ErrorSendMessage
+	ErrorS3
 )
 
 var stuff struct {
-	queueId, region string
-	sqs             *sqs.SQS
-	auth            aws.Auth
+	regionName            string
+	queueName, bucketName string
+	region                aws.Region
+	sqs                   *sqs.SQS
+	auth                  aws.Auth
 
 	items data.ItemSlice
+	queue *sqs.Queue
+}
+
+func Color(color ct.Color, msg ...interface{}) {
+	ct.ChangeColor(color, false, ct.None, false)
+	fmt.Print(msg...)
+	ct.ResetColor()
+}
+func Colorln(color ct.Color, msg ...interface{}) {
+	msg2 := make([]interface{}, 0, len(msg)*2-1)
+	for i, s := range msg {
+		if i != 0 {
+			msg2 = append(msg2, " ")
+		}
+		msg2 = append(msg2, s)
+	}
+	Color(color, msg2...)
+	fmt.Println()
+}
+func Redln(msg ...interface{}) {
+	Colorln(ct.Red, msg...)
+}
+func Yellow(msg ...interface{}) {
+	Color(ct.Yellow, msg...)
 }
 
 func main() {
@@ -41,8 +70,9 @@ func main() {
 	flag.StringVar(&jsonSource, "json", "", "Source: JSON file with array of items")
 	flag.StringVar(&tsvSource, "tsv", "", `Source: tsv file with format "video id\tTitle"`)
 
-	flag.StringVar(&stuff.queueId, "queue", "", "SQS queue ID")
-	flag.StringVar(&stuff.region, "region", "us-east-1", "AWS Region")
+	flag.StringVar(&stuff.queueName, "queue", "", "SQS Queue Name")
+	flag.StringVar(&stuff.bucketName, "bucket", "", "S3 Bucket Name (only for receive)")
+	flag.StringVar(&stuff.regionName, "region", "us-east-1", "AWS Region")
 
 	flag.Parse()
 	if help {
@@ -50,17 +80,22 @@ func main() {
 		os.Exit(ErrorArgs)
 	}
 	if !(doReceive || doSend) || (doReceive && doSend) {
-		fmt.Println("Please select receive OR send")
+		Redln("Please select receive OR send.")
 		flag.Usage()
 		os.Exit(ErrorArgs)
 	}
-	if stuff.queueId == "" {
-		fmt.Println("Please set queue ID")
+	if stuff.queueName == "" {
+		Redln("Please set queue ID.")
 		flag.Usage()
 		os.Exit(ErrorArgs)
 	}
 	if doSend && (jsonSource == "" && tsvSource == "") {
-		fmt.Println("Please specify JSON or tsv source.")
+		Redln("Please specify JSON or tsv source.")
+		flag.Usage()
+		os.Exit(ErrorArgs)
+	}
+	if doReceive && stuff.bucketName == "" {
+		Redln("Please specify S3 bucket name for receive.")
 		flag.Usage()
 		os.Exit(ErrorArgs)
 	}
@@ -72,26 +107,35 @@ func main() {
 		var u *user.User
 		u, err = user.Current()
 		if err != nil {
-			fmt.Println("Could not get User dir and", oldErr)
+			Redln("Could not get User dir and", oldErr)
 			os.Exit(ErrorAwsAuth)
 		}
 		stuff.auth, err = aws.CredentialFileAuth(u.HomeDir+"/.aws/credentials", "default", 0)
 		if err != nil {
-			fmt.Println("Environment aws auth failed:", oldErr, "- File failed:", err)
+			Redln("Environment aws auth failed:", oldErr, "- File failed:", err)
 			os.Exit(ErrorAwsAuth)
 		}
 	}
 
-	re, ok := aws.Regions[stuff.region]
+	var ok bool
+	stuff.region, ok = aws.Regions[stuff.regionName]
 	if !ok {
-		fmt.Println("Region", stuff.region, "not supported. Available are:")
+		Redln("Region", stuff.regionName, "not supported. Available are:")
 		for k := range aws.Regions {
 			fmt.Print(k, ", ")
 		}
-		fmt.Println()
+		Redln()
 		os.Exit(ErrorArgs)
 	}
-	stuff.sqs = sqs.New(stuff.auth, re)
+	stuff.sqs = sqs.New(stuff.auth, stuff.region)
+
+	stuff.queue, err = stuff.sqs.GetQueue(stuff.queueName)
+	if err != nil {
+		Redln("GetQueue:", err)
+		os.Exit(ErrorGetQueue)
+	}
+	Yellow("Queue url: ")
+	fmt.Println(stuff.queue.Url)
 
 	if doSend {
 		if jsonSource != "" {
@@ -100,7 +144,7 @@ func main() {
 			err = stuff.items.LoadTSV(tsvSource)
 		}
 		if err != nil {
-			fmt.Println("Loading JSON/CSV:", err)
+			Redln("Loading JSON/CSV:", err)
 			os.Exit(ErrorSource)
 		}
 		send()
@@ -108,27 +152,33 @@ func main() {
 	if doReceive {
 		receive()
 	}
-	fmt.Println("finished.")
+	Redln("finished.")
 }
 
 func receive() {
-	fmt.Println("Error: not implemented")
+	s3client := s3.New(stuff.auth, stuff.region)
+	/* I haz a */ bucket := s3client.Bucket(stuff.bucketName)
+	res, err := bucket.List("", "", "", 100)
+	if err != nil {
+		Redln("S3 list:", err)
+		os.Exit(ErrorS3)
+	}
+	Redln("res:", res.Contents)
+
+	//TODO
+	Redln("Error: not implemented")
 	os.Exit(ErrorNotImpl)
 }
 
 func send() {
-	queue, err := stuff.sqs.GetQueue(stuff.queueId)
-	if err != nil {
-		fmt.Println("GetQueue:", err)
-		os.Exit(ErrorGetQueue)
-	}
 	for _, item := range stuff.items {
 		msg := item.Code + "\t" + item.Title
-		resp, err := queue.SendMessage(msg)
+		resp, err := stuff.queue.SendMessage(msg)
 		if err != nil {
-			fmt.Println("SendMessage:", err)
+			Redln("SendMessage:", err)
 			os.Exit(ErrorSendMessage)
 		}
-		fmt.Println("Sent", msg, "id:", resp.Id, "md5:", resp.MD5)
+		Yellow("Sent", msg)
+		fmt.Println("id:", resp.Id, "md5:", resp.MD5)
 	}
 }
